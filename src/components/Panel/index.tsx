107e413887ae32ccf8676b5dd456ae2d2c4fdf6f
高伟 <15882883832@163.com>
2025-12-29 17:28:39 +0800

    1

--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -7,9 +7,15 @@ interface PanelProps {
   className?: string;
 }
 
-const Panel = ({ children, className = '' }: PanelProps) => {
+const Panel = ({ title, children, className = '' }: PanelProps) => {
   return (
     <div className={`tech-panel ${className}`}>
+      {title && (
+        <div className="panel-title">
+          <span className="title-text">{title}</span>
+          <div className="title-decoration"></div>
+        </div>
+      )}
       <div className="panel-content">
         {children}
       </div>
